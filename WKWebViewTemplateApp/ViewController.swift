e5eafa499694b2e41d2ce991fee87ed88098cd37
Sam Heather <sam@heather.sh>
2021-05-04 20:51:58 +0100

    Initial commit

--- WKWebViewTemplateApp/ViewController.swift
+++ WKWebViewTemplateApp/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var urlBar:UITextField!
+    @IBOutlet var goBUtton:UIButton!
+    var webView:PWKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://facebook.com/")
+        let request = URLRequest(url: url!)
+        
+        var frame:CGRect = view.frame
+        frame.origin.y = 120
+        webView = PWKWebView(frame: frame)
+//        let webView = WKWebView(frame: frame, configuration: WKWebViewConfiguration.proxyConifg)
+        view.addSubview(webView)
+        
+        webView?.load(request)
+    }
+    
+    @IBAction func goButtonPressed(sender: AnyObject) {
+        let url = URL(string: urlBar.text!)
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
 
 
